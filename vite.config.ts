dfb52c4fb3e36c98d00135ad31d87a9795a5bbae
parasharakash2108-dot <parasharakash2108@gmail.com>
2025-09-29 16:53:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server:{
+    port:5177
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/widget.tsx'), 
+      name: 'ReactWidget',
+      fileName: 'react-widget-mabel',
+      formats: ['iife'], 
+    },
+    rollupOptions: {
+      output: { 
+      },
+    },
   },
 });
